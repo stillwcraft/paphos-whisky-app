bfaf0c4954bbf646602eabe51e6ffb1b573ca9c4
antonbuzhynskyi <anton.buzhinskyi@uklon.com.ua>
2026-09-19 13:54:41 +0300

    Feature: fix map

--- frontend/src/components/ScotlandWhiskyMap.tsx
+++ frontend/src/components/ScotlandWhiskyMap.tsx
@@ -265,6 +265,7 @@ export function ScotlandWhiskyMap({
           zoom={position.zoom}
           minZoom={1}
           maxZoom={16}
+          disablePanning={position.zoom <= initialPosition.zoom}
           onMoveEnd={setPosition}
         >
           <g style={{ filter: 'drop-shadow(0px 10px 25px rgba(0, 0, 0, 0.9))' }}>
